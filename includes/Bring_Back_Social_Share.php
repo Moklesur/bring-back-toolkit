109e373b8a3e3a7fe65c7a33a4e316df02c6bff6
mokleusr <mokelsur42@gmail.com>
2019-09-25 21:01:13 +0600

    social share class

---
+++ includes/Bring_Back_Social_Share.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Bring_Back_Social_Share
+ */
+
+class Bring_Back_Social_Share
+{
+   public $private = 'social share';
+
+   function social_share(){
+       echo $this->private;
+   }
+}
