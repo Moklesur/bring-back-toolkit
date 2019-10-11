4b4c7ed69cb247d00f211663bd48d54c812b1743
mokleusr <mokelsur42@gmail.com>
2019-10-11 11:21:43 +0600

    breadcrumbs

--- bring-back-toolkit.php
+++ bring-back-toolkit.php
@@ -71,6 +71,8 @@ if( ! class_exists('Bring_Back_Toolkit') ) {
             add_action('save_post', array( $this, 'case_studies_save_metabox' ) );
             // Social Share
             add_filter( 'the_content', array( $this, 'bb_social_share' ) );
+            // Breadcrumbs
+            add_shortcode( 'bb_breadcrumbs', array( $this, 'bring_back_breadcrumbs' ) );
         }
 
         /**
@@ -94,7 +96,7 @@ if( ! class_exists('Bring_Back_Toolkit') ) {
             $this->define( 'Bring_Back_Toolkit_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
             $this->define( 'Bring_Back_Toolkit_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
             $this->define( 'Bring_Back_Toolkit_VERSION', $this->version );
-            //$this->define( 'Bring_Back_Toolkit_PLUGIN_INCLUDE_PATH', trailingslashit( plugin_dir_path( __FILE__ ) . 'includes' ) );
+            $this->define( 'Bring_Back_Toolkit_PLUGIN_INCLUDE_PATH', trailingslashit( plugin_dir_path( __FILE__ ) . 'includes' ) );
             $this->define( 'Bring_Back_Toolkit_PLUGIN_DIRNAME', dirname( plugin_basename( __FILE__ ) ) );
             $this->define( 'Bring_Back_Toolkit_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
         }
@@ -349,6 +351,143 @@ if( ! class_exists('Bring_Back_Toolkit') ) {
                     'class' => 'lin'
                 ]
             ];
+        }
+
+
+        /**
+         * @param $args
+         * @return string|void
+         *
+         * Breadcrumbs
+         */
+        public function bring_back_breadcrumbs( $args ) {
+
+            if ( is_front_page() ) {
+                return;
+            }
+
+            global $post;
+            $defaults  = array(
+                'separator_icon'      => '',
+                'breadcrumbs_id'      => 'breadcrumb',
+                'breadcrumbs_classes' => ' text-center text-capitalize',
+                'home_title'          => esc_html__( 'Home', 'bring-back' )
+            );
+            $args      = apply_filters( 'bring_back_breadcrumbs_args', wp_parse_args( $args, $defaults ) );
+
+            // Open the breadcrumbs
+            $html = '<div class="breadcrumb-wrapper"><div id="' . esc_attr( $args['breadcrumbs_id'] ) . '" class="' . esc_attr( $args['breadcrumbs_classes'] ) . '"><div class="col-12">';
+
+            $title = '<h1 class="page-title">'.get_the_title().'</h1>';
+
+            // Add Homepage link & separator (always present)
+            $home = '<span class="item-home"><a class="bread-link bread-home" href="' . esc_url(get_home_url()) . '" title="' . esc_attr( $args['home_title'] ) . '">' .  esc_html($args['home_title']) . '</a></span>';
+
+            // Post
+            if ( is_singular( 'post' ) ) {
+
+                $category = get_the_category();
+                $category_values = array_values( $category );
+                $last_category = end( $category_values );
+                $cat_parents = rtrim( get_category_parents( $last_category->term_id, true, ',' ), ',' );
+                $cat_parents = explode( ',', $cat_parents );
+                $html .= $title;
+                $html .= $home;
+                foreach ( $cat_parents as $parent ) {
+                    $html .= '<span class="item-cat">' . wp_kses( $parent, wp_kses_allowed_html( 'a' ) ) . '</span>';
+                }
+
+                $html .= '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . esc_attr( get_the_title() ) . '">' . esc_html( get_the_title() ) . '</span></span>';
+            } elseif ( is_singular( 'page' ) ) {
+                $html .= $title;
+                $html .= $home;
+                if ( $post->post_parent ) {
+                    $parents = get_post_ancestors( $post->ID );
+                    $parents = array_reverse( $parents );
+
+                    foreach ( $parents as $parent ) {
+                        $html .= '<span class="item-parent item-parent-' . esc_attr( $parent ) . '"><a class="bread-parent bread-parent-' . esc_attr( $parent ) . '" href="' . esc_url( get_permalink( $parent ) ) . '" title="' . esc_attr( get_the_title( $parent ) ) . '">' . esc_html( get_the_title( $parent ) ) . '</a></span>';
+
+                    }
+                }
+                $html .= '<span class="item-current item-' . $post->ID . '"><span title="' . esc_attr( get_the_title() ) . '"> ' . esc_html( get_the_title() ) . '</span></span>';
+            } elseif ( is_singular( 'attachment' ) ) {
+
+                $parent_id        = $post->post_parent;
+                $parent_title     = get_the_title( $parent_id );
+                $parent_permalink = esc_url( get_permalink( $parent_id ) );
+                $html .= $title;
+                $html .= $home;
+                $html .= '<span class="item-parent"><a class="bread-parent" href="' . esc_url( $parent_permalink ) . '" title="' . esc_attr( $parent_title ) . '">' . esc_html( $parent_title ) . '</a></span>';
+
+                $html .= '<span class="item-current item-' . $post->ID . '"><span title="' . esc_attr( get_the_title() ) . '"> ' . esc_html( get_the_title() ) . '</span></span>';
+            } elseif ( is_singular() ) {
+
+                $post_type         = get_post_type();
+                $post_type_object  = get_post_type_object( $post_type );
+                $post_type_archive = get_post_type_archive_link( $post_type );
+                $html .= $title;
+                $html .= $home;
+                $html .= '<span class="item-cat item-custom-post-type-' . esc_attr( $post_type ) . '"><a class="bread-cat bread-custom-post-type-' . esc_attr( $post_type ) . '" href="' . esc_url( $post_type_archive ) . '" title="' . esc_attr( $post_type_object->labels->name ) . '">' . esc_html( $post_type_object->labels->name ) . '</a></span>';
+
+                $html .= '<span class="item-current item-' . $post->ID . '"><span class="bread-current bread-' . $post->ID . '" title="' . esc_attr($post->post_title) . '">' . esc_html($post->post_title) . '</span></span>';
+            } elseif ( is_category() ) {
+
+                $parent = get_queried_object()->category_parent;
+
+                if ( $parent !== 0 ) {
+
+                    $parent_category = get_category( $parent );
+                    $category_link   = get_category_link( $parent );
+
+                    $html .= '<span class="item-parent item-parent-' . esc_attr( $parent_category->slug ) . '"><a class="bread-parent bread-parent-' . esc_attr( $parent_category->slug ) . '" href="' . esc_url( $category_link ) . '" title="' . esc_attr( $parent_category->name ) . '">' . esc_html( $parent_category->name ) . '</a></span>';
+
+                }
+                $html .= '<h1 class="page-title">'.single_cat_title( '', false ).'</h1>';;
+                $html .= $home;
+                $html .= '<span class="item-current item-cat"><span class="bread-current bread-cat" title="' . esc_attr($post->ID) . '">' . single_cat_title( '', false ) . '</span></span>';
+            } elseif ( is_tag() ) {
+                $html .= '<h1 class="page-title">'.single_tag_title( '', false ).'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-tag"><span class="bread-current bread-tag">' . single_tag_title( '', false ) . '</span></span>';
+            } elseif ( is_author() ) {
+                $html .= '<h1 class="page-title">'.esc_html( get_queried_object()->display_name  ).'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-author"><span class="bread-current bread-author">' . get_queried_object()->display_name . '</span></span>';
+            } elseif ( is_day() ) {
+                $html .= '<h1 class="page-title">'.get_the_date().'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-day"><span class="bread-current bread-day">' . get_the_date() . '</span></span>';
+            } elseif ( is_month() ) {
+                $html .= '<h1 class="page-title">'.get_the_date( 'F Y' ).'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-month"><span class="bread-current bread-month">' . get_the_date( 'F Y' ) . '</span></span>';
+            } elseif ( is_year() ) {
+                $html .= '<h1 class="page-title">'.get_the_date( 'Y' ).'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-year"><span class="bread-current bread-year">' . get_the_date( 'Y' ) . '</span></span>';
+            } elseif ( is_archive() ) {
+                $custom_tax_name = get_queried_object()->name;
+                $html .= '<h1 class="page-title">'.esc_html( $custom_tax_name ).'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-archive"><span class="bread-current bread-archive">' . esc_html( $custom_tax_name ) . '</span></span>';
+            } elseif ( is_search() ) {
+                $html .= '<h1 class="page-title">'.get_search_query().'</h1>';
+                $html .= $home;
+                $html .= '<span class="item-current item-search"><span class="bread-current bread-search">'.__('Search results for : ','bring-back').' ' . get_search_query() . '</span></span>';
+            } elseif ( is_404() ) {
+                $html .= '<h1 class="page-title">'.esc_html( '404' ).'</h1>';
+                $html .= $home;
+                $html .= '<span>' . __( 'Error 404', 'bring-back' ) . '</span>';
+            } elseif ( is_home() ) {
+                $html .= $home;
+                $html .= '<span>' . esc_html( get_the_title( get_option( 'page_for_posts' ) ) ) . '</span>';
+            }
+
+            $html .= '</div></div></div>';
+            $html = apply_filters( 'bring_back_breadcrumbs_filter', $html );
+
+            return wp_kses_post( $html );
         }
 
     }
